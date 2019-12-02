f99ac72479a7a660c57ae653667938c697d50341
Aliakwe <aliakwe@yahoo.com>
2019-12-02 09:53:09 +0100

    Updates

---
+++ precache-manifest.86e16d9c091c15020baf8745ebdc9915.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1da20cd38772a350947c4a44c400d8df",
+    "url": "/reduxcontactmanager/index.html"
+  },
+  {
+    "revision": "2794c534a99a416ce700",
+    "url": "/reduxcontactmanager/static/css/main.094e43e2.chunk.css"
+  },
+  {
+    "revision": "0d938a3d3082b3e9a67b",
+    "url": "/reduxcontactmanager/static/js/2.aaf611f8.chunk.js"
+  },
+  {
+    "revision": "2794c534a99a416ce700",
+    "url": "/reduxcontactmanager/static/js/main.f6cbbac9.chunk.js"
+  },
+  {
+    "revision": "117486ca542b91f0629b",
+    "url": "/reduxcontactmanager/static/js/runtime-main.3d6de1f2.js"
+  }
+]);
